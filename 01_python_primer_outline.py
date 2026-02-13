742190ad2191ba83974d52a18c499871f280a2c5
sean-blessing <snblessing@gmail.com>
2026-02-13 13:02:27 -0500

    init commit

---
+++ 01_python_primer_outline.py
@@ -0,0 +1,28 @@
+# Python naming conventions
+# PEP 8 - https://peps.python.org/pep-0008/
+# Use underscores to separate words!
+# lower_case_with_underscores
+
+# p. 6 print function / hello world
+
+# p. 8 declare variables / data types
+
+# arithmetic operators
+
+# concatenate variables w/ + operator
+
+# print vs printf
+
+# print w/ default separator (,)
+
+# print w/ 'sep' keyword
+
+# print w/ 'end' keyword
+
+# input function
+
+# convert to numeric
+
+# p. 9 if statements
+
+# while loop
